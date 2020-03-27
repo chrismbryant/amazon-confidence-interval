59ccdf38185c7a44dd40d81deb776677fdd71f5b
nicholas <musicin3d@gmail.com>
2020-03-26 23:59:12 -0500

    Trigger distribution-based confidence interval on-scroll-into-view. Skip products that have already been handled.

--- src/chrome/inject.js
+++ src/chrome/inject.js
@@ -2,13 +2,70 @@ import calculations from "../shared/calculations"
 import scraper from "../shared/scraper"
 
 
-(async ()=> {
+let noDistProducts = []
+let scrollTicking = false
+
+
+;(async ()=> {
     const products = await scraper.findAllProducts()
     for (let product of products) {
         if(!product.reviewCount) continue
-        const confidenceDOM = document.createElement("div");
-        const ci = calculations.evaluateAverageRating(product.rating, product.reviewCount)
-        confidenceDOM.innerHTML = `CI for score ${product.rating}, n=${product.reviewCount} is <br> proportion: ${ci.proportion} <br> lower: ${ci.lower} <br> upper: ${ci.upper}`;
-        product.dom.insertAdjacentElement("beforeend", confidenceDOM);
+        dumpAvgConfidence(product)
     }
+    noDistProducts = products
+    window.addEventListener('resize', handleScroll)
+    window.addEventListener('scroll', handleScroll)
+    handleScroll()
 })()
+
+
+
+function dumpAvgConfidence(product){
+    const confidenceDOM = document.createElement("div");
+    const ci = calculations.evaluateAverageRating(product.rating, product.reviewCount)
+    confidenceDOM.innerHTML = `CI for score ${product.rating}, n=${product.reviewCount} is <br> proportion: ${ci.proportion} <br> lower: ${ci.lower} <br> upper: ${ci.upper}`;
+    product.dom.insertAdjacentElement("beforeend", confidenceDOM);
+}
+
+
+function handleScroll(evt){
+    if(scrollTicking)  return
+    scrollTicking = true
+    window.requestAnimationFrame(()=>{
+        const leftover = []
+        for(let product of noDistProducts) {
+            if (productInView(product)){
+                // noinspection JSIgnoredPromiseFromCall
+                dumpDistConfidence(product)
+            }
+            else {
+                leftover.push(product)
+            }
+        }
+        noDistProducts = leftover
+        scrollTicking = false
+    })
+}
+
+
+async function dumpDistConfidence(product){
+    scraper.getDistribution(product)
+    product.dom.style.backgroundColor = 'blue'
+}
+
+
+function productInView(product){
+    const rect = product.dom.getBoundingClientRect();
+    const view = {
+        top: 0,
+        left: 0,
+        right: window.innerWidth,
+        bottom: window.innerHeight
+    }
+    return !(
+        rect.right < view.left
+        || rect.left > view.right
+        || rect.bottom < view.top
+        || rect.top > view.bottom
+    );
+}
--- src/shared/scraper.js
+++ src/shared/scraper.js
@@ -4,6 +4,7 @@
  *  @property dom  DOM element that contains the relevant data
  *  @property {number} rating  Average rating. If there are not reviews, this will be null.
  *  @property {number} reviewCount  Number of reviews
+ *  @property {array} distributions  Fractions of each star rating over all ratings. Indices 0-5 => 1-5 stars
  */
 
 
@@ -23,16 +24,25 @@ export default {
             function run(){
                 const images = document.querySelectorAll('[data-component-type=s-product-image]')
                 const products = [...images].map(image => image.closest('.a-section'))
-                resolve(products.map(product =>{
+                resolve( products.map(product =>{
                     const stats = getStats(product)
                     return {
                         dom: product,
                         rating: stats.rating,
-                        reviewCount: stats.reviewCount
+                        reviewCount: stats.reviewCount,
+                        distributions: []
                     }
-                }))
+                }) )
             }
         })
+    },
+
+    /**
+     * Retrieves the rating distribution for the given product
+     * @param {Product} product
+     */
+    getDistribution(product){
+        console.log(product.dom.querySelector('[data-a-popover]').dataset.aPopover)
     }
 
 }
@@ -41,7 +51,7 @@ export default {
 
 function getStats(product) {
     let rating, reviewCount
-    const section = product.querySelector(".a-section.a-spacing-none.a-spacing-top-micro>.a-row.a-size-small")
+    const section = product.querySelector(".a-section.a-spacing-none.a-spacing-top-micro > .a-row.a-size-small")
     if(section){
         rating = product.querySelector("span.a-icon-alt").innerText.split(" out of")[0] * 1
         reviewCount = parseFloat(product.querySelector("span.a-size-base").innerText.replace(/[,.]/g, ""))
